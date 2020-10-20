ad9895a5fa0da2a33c26e84084d3f1fdb933a3a3
AndreaScacchi <69596559+AndreaScacchi@users.noreply.github.com>
2020-10-20 19:31:31 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@ const navbar = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
 
+// add class 'active' to section when it is near top of viewport
+function makeActive() {
+    for (const section of sections) {
+        const item = section.getBoundingClientRect();
+        if (item.top <= 150 && item.bottom >= 150) {
+           
+        }
+    }
+}
+
+
+
 // build the navbar
 for (let section of sections) {
     const listElement = document.createElement('li');
