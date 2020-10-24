ef29f6591bf10865acc5929e931f98e5ae0a0c30
AndreaScacchi <69596559+AndreaScacchi@users.noreply.github.com>
2020-10-24 11:29:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
 //define global variables
-const navbar = document.getElementById('navbar__list');
-const sections = document.querySelectorAll('section');
-
+const navbar = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
 
 // add class 'active' to section when it is near top of viewport
 function makeActive() {
     for (const section of sections) {
         const item = section.getBoundingClientRect();
         if (item.top <= 150 && item.bottom >= 150) {
-            section.classList.add('your-active-class');
-        }else {
-            section.classList.remove('your-active-class');
+            section.classList.add("your-active-class");
+        } else {
+            section.classList.remove("your-active-class");
         }
     }
 }
-document.addEventListener('scroll', function () {
+document.addEventListener("scroll", function () {
     makeActive();
 });
-
-
 
 //build navbar dynamically
 function createNav() {
@@ -27,23 +24,20 @@ function createNav() {
         myNav.className = "menu__link";
         myNav.dataset.nav = item.id;
         myNav.innerText = item.dataset.nav;
-        myNav.addEventListener('click', function () {
-            item.scrollIntoView({behavior: "smooth"})
-        })
+        myNav.addEventListener("click", function () {
+            item.scrollIntoView({ behavior: "smooth" });
+        });
         navbar.appendChild(myNav);
     }
 }
 createNav();
 
-
 //build the button
-const myButton = document.querySelector('.btn');
+const myButton = document.querySelector(".btn");
 
 function theButton() {
-    myButton.addEventListener('click', function () {
-        window.scrollTo({behavior: 'smooth',
-    top: 0});
+    myButton.addEventListener("click", function () {
+        window.scrollTo({ behavior: "smooth", top: 0 });
+    });
 }
-)};
-
 theButton();
